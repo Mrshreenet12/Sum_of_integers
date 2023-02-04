f33ab9281794779dfb1db54ca98900b4ebae441e
Mrshreenet12 <124419930+Mrshreenet12@users.noreply.github.com>
2023-02-04 11:11:42 +0530

    Add files via upload

---
+++ Sum_0f_integers.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2; 
+    
+    printf("Enter First Number: ");
+    scanf("%d",&num1);
+    
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+    
+    printf("The sum of %d and %d is %d.",num1,num2,num1+num2);
+   
+
+    return 0;
+}
